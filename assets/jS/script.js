672b1b6a5f75f9b429d4f2ad647f136e8bfcf07f
“Gigi <debortolg@gmail.com>
2022-09-09 18:01:32 -0700

    adding new task to the list with a button click

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -1,21 +1,18 @@
-// Button
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-buttonEl.addEventListener("click", function () {
-  alert("button clicked");
-});
-
 // Adding DOM object reference so that when we add a new task it lets us create one
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-//Event listener
-buttonEl.addEventListener("click", function () {
-  // created new item
-  var listItemEl = document.createElement("li");
+
+// Task Handler - must be before the other content or the var would be null
+var createTaskHandler = function() {
+   // created new item
+  var listItemEl =document.createElement("li");
   listItemEl.className = "task-item";
   // add the task/ changes the text 
   listItemEl.textContent = "This is a new task.";
-  // style the item to match and append it to the end of the task list
+   // style the item to match and append it to the end of the task list
   tasksToDoEl.appendChild(listItemEl);
-});
+};
+// Event Listener
+buttonEl.addEventListener("click", createTaskHandler);
+
+
