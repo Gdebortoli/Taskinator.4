12a3edab8606dd4fc19a16fee653d4a37aba34c2
“Gigi <debortolg@gmail.com>
2022-09-16 12:47:31 -0700

    using console to make sure its functioning properly

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -1,9 +1,10 @@
 // Adding DOM object reference so that when we add a new task it lets us create one
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Task Handler - must be before the other content or the var would be null
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault();
    // created new item
   var listItemEl =document.createElement("li");
   listItemEl.className = "task-item";
@@ -12,7 +13,8 @@ var createTaskHandler = function() {
    // style the item to match and append it to the end of the task list
   tasksToDoEl.appendChild(listItemEl);
 };
-// Event Listener
-buttonEl.addEventListener("click", createTaskHandler);
+
+// Event Listener - Changed from buttonEL to listen to entire form and not just button function
+formEl.addEventListener("submit", createTaskHandler);
 
 
