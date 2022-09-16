9d12515551d961b20135d6e48e5c15489ae1dad0
“Gigi <debortolg@gmail.com>
2022-09-16 14:28:11 -0700

    able to add values and new tasks - needs refractoring

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -5,12 +5,23 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // Task Handler - must be before the other content or the var would be null
 var createTaskHandler = function(event) {
   event.preventDefault();
-   // created new item
+   // reading tasks entered input from form 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+   // get the value of the driopdown to show up below task 
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+   // create list item
   var listItemEl =document.createElement("li");
   listItemEl.className = "task-item";
-  // add the task/ changes the text 
-  listItemEl.textContent = "This is a new task.";
-   // style the item to match and append it to the end of the task list
+
+  // create a div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name 
+  taskInfoEl.className = "task-info";
+  // add html content to div
+  taskInfoEl.innerHTML = "<h3 class='tasl-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  // appended entire li to the parent ul(taskToDoEl)
+  listItemEl.appendChild(taskInfoEl);
+   // style the list item to match and append the list item to add to the list 
   tasksToDoEl.appendChild(listItemEl);
 };
 
