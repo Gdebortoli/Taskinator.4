42ba0a889d809ff76e93dc8fb21e61920577f4c5
“Gigi <debortolg@gmail.com>
2022-09-16 18:39:59 -0700

    refractored code

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -3,14 +3,24 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Task Handler - must be before the other content or the var would be null
-var createTaskHandler = function(event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
-   // reading tasks entered input from form 
+  // reading tasks entered input from form 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
-   // get the value of the driopdown to show up below task 
+  // get the value of the driopdown to show up below task 
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-   // create list item
-  var listItemEl =document.createElement("li");
+  // package up data as an object 
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
   // create a div to hold task info and add to list item
@@ -18,14 +28,13 @@ var createTaskHandler = function(event) {
   // give it a class name 
   taskInfoEl.className = "task-info";
   // add html content to div
-  taskInfoEl.innerHTML = "<h3 class='tasl-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='tasl-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   // appended entire li to the parent ul(taskToDoEl)
   listItemEl.appendChild(taskInfoEl);
-   // style the list item to match and append the list item to add to the list 
+  // style the list item to match and append the list item to add to the list 
   tasksToDoEl.appendChild(listItemEl);
 };
 
 // Event Listener - Changed from buttonEL to listen to entire form and not just button function
-formEl.addEventListener("submit", createTaskHandler);
-
+formEl.addEventListener("submit", taskFormHandler);
 
